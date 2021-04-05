29fa41d5d82037f7dc1ddb235408e2317bd8151c
YORESTARLING MEJIA BERAS <ym2018-0345@uce.edu.do>
2021-04-04 22:38:24 -0400

    Conexion Correcta Web

---
+++ DataAccess2/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace DataAccess2
+{
+    public class Class1
+    {
+    }
+}
--- SysMediPlusWeb/Startup.cs
+++ SysMediPlusWeb/Startup.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -8,6 +9,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using DataAccess;
 
 namespace SysMediPlusWeb
 {
@@ -23,7 +25,13 @@ namespace SysMediPlusWeb
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            
+            services.AddDbContext<MediPlusSysContext>((options) =>
+            {
+                options.UseSqlServer(Configuration.GetConnectionString("MediPlusSysAdv"));
+            });
             services.AddControllersWithViews();
+            
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
